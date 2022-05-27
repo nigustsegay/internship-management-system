5ea2d4fa498ee572574bf4eb5cbc4fa222c0fdc6
wesentalem <59748935+wesentalem@users.noreply.github.com>
2022-05-27 21:22:09 +0300

    Delete web/src directory

--- web/src/pages/RoleRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
-const Layout = ({ role: requiredRole }) => {
-    const { metadata: { role } } = useAuth();
-    return (<>{requiredRole === role ? <Outlet /> : <Navigate to="/unauthorized" replace />}</>);
-}
-export default Layout;
