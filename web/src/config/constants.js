7f15de41aeb5c734b026d309be5b724b7b52f388
wesentalem <59748935+wesentalem@users.noreply.github.com>
2022-05-27 21:18:05 +0300

    Delete web/src/config directory

--- web/src/config/constants.js
+++
@@ -1,9 +0,0 @@
-export const departments = [
-    { id: 'SITE', label: 'School of Information Technology and Engineering' },
-    { id: 'SCEE', label: 'School of Civil & Enviromental Engineering' },
-    { id: 'SECE', label: 'School of Electrical & Computer Engineering' },
-    { id: 'SMIE', label: 'School of Mechanical and Industrial Engineering' },
-    { id: 'SCBE', label: 'School of Chemical and Bio Engineering' },
-    { id: 'SMDE', label: 'School of Multi-disciplinary Engineering' },
-
-]
