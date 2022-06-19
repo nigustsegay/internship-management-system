5da70c856bf41fbc498121894abd786f483ded3e
NattyKindu <56720565+NattyKindu@users.noreply.github.com>
2022-06-19 11:52:51 -0700

    Update Login.jsx

--- web/src/components/Login.jsx
+++ web/src/components/Login.jsx
@@ -9,7 +9,48 @@ const commonSchema = {
     email: Yup.string().email().required('Email is required'),
     password: Yup.string().required('Password is required'),
 };
+componentDidCatch(error, errorInfo) {
+        // Catch errors in any components below and re-render with error message
+        this.setState({
+            error: error,
+            errorInfo: errorInfo
+        })
+        // You can also log error messages to an error reporting service here
+    } Assign
+            </Button>
+            {onReject && <Button
+                type="button"
+                id="save"
+                onClick={() => {
+                    onReject();
+                }}
+                isLoading={isLoading}
+                overrides={{
+                    BaseButton: {
+                        style: ({ $theme }) => ({
+                            marginLeft: $theme.sizing.scale200,
+                            marginRight: $theme.sizing.scale200,
 
+
+    render() {
+        if (this.state.errorInfo) {
+            // Error path
+            return (
+                <div>
+                    <h2>Something went wrong.</h2>
+                    <details style={{ whiteSpace: 'pre-wrap' }}>
+                        {this.state.error && this.state.error.toString()}
+                        <br />
+                        {this.state.errorInfo.componentStack}
+                    </details>
+                </div>
+            );
+        }
+        }
+        // Normally, just render children
+        return this.props.children;
+    }
+}
 const Component = ({
     email: initialEmail,
     password: initialPassword,
@@ -68,7 +109,48 @@ const Component = ({
                     handleSubmit();
                 }}
                 isLoading={isLoading}
-            >
+            >componentDidCatch(error, errorInfo) {
+        // Catch errors in any components below and re-render with error message
+        this.setState({
+            error: error,
+            errorInfo: errorInfo
+        })
+        // You can also log error messages to an error reporting service here
+    } Assign
+            </Button>
+            {onReject && <Button
+                type="button"
+                id="save"
+                onClick={() => {
+                    onReject();
+                }}
+                isLoading={isLoading}
+                overrides={{
+                    BaseButton: {
+                        style: ({ $theme }) => ({
+                            marginLeft: $theme.sizing.scale200,
+                            marginRight: $theme.sizing.scale200,
+
+
+    render() {
+        if (this.state.errorInfo) {
+            // Error path
+            return (
+                <div>
+                    <h2>Something went wrong.</h2>
+                    <details style={{ whiteSpace: 'pre-wrap' }}>
+                        {this.state.error && this.state.error.toString()}
+                        <br />
+                        {this.state.errorInfo.componentStack}
+                    </details>
+                </div>
+            );
+        }
+        }
+        // Normally, just render children
+        return this.props.children;
+    }
+}
                 Login
             </Button>
         </>
